0379e13e317cadc415a9a35959b62551abd5635b
VIVIAN RION <vivianrionmarandi@gmail.com>
2024-03-27 20:58:11 +0600

    pages to read and dynamic bar chart uploaded

--- src/components/PagestoRead/PagestoRead.jsx
+++ src/components/PagestoRead/PagestoRead.jsx
@@ -1,10 +1,37 @@
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+import { getLocalStoreItem } from "../Utility/Utility";
 
 const PagestoRead = () => {
+
+    const getReadBooksData = getLocalStoreItem();
+
     return (
-        <div>
+        <div className='container mx-auto mt-32 '>
 
-            <h1>Pages to Read</h1>
+            <div className=''>
 
+                <BarChart
+                    width={1200}
+                    height={575}
+                    data={getReadBooksData}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="bookName" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="totalPages" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                </BarChart>
+
+            </div>
+            
         </div>
     );
 };
